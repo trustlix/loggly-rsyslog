e6c45a2a078487c6c683f6e2826a089121805f1c
Lucas Souza <lucasas@gmail.com>
2014-04-01 17:24:25 -0300

    using deploy recipe to know application

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,5 @@ license          'Apache 2.0'
 description      'Configures rsyslog to send logs to Loggly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
-
-supports 'ubuntu', '>= 12.04'
-
+supports         'ubuntu', '>= 12.04'
+depends          'deploy'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,22 +7,28 @@
 # All rights reserved - Do Not Redistribute
 #
 
-loggly_token = data_bag_item('loggly', 'token')
-raise "No token was found in the loggly databag." if loggly_token.nil?
+include_recipe 'deploy'
 
-service "rsyslog"
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
 
-include_recipe "loggly-rsyslog::tls" if node['loggly']['tls']['enabled']
+  loggly_token = data_bag_item(application, 'loggly')['token']
+  raise "No token was found in the loggly databag." if loggly_token.nil?
 
-template '/etc/rsyslog.conf' do
-  source 'loggly.conf.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
-  variables({
-    :monitor_files => !node['loggly']['log_files'].empty? || !node['loggly']['log_dirs'].empty?,
-    :tags => node['loggly']['tags'].nil? || node['loggly']['tags'].empty? ? '' : "tag=\\\"#{node['loggly']['tags'].join("\\\" tag=\\\"")}\\\"",
-    :token => loggly_token
-  })
-  notifies :restart, "service[rsyslog]", :immediate
+  service "rsyslog"
+
+  include_recipe "loggly-rsyslog::tls" if node['loggly']['tls']['enabled']
+
+  template '/etc/rsyslog.conf' do
+    source 'loggly.conf.erb'
+    owner 'root'
+    group 'root'
+    mode 0644
+    variables({
+      :monitor_files => !node['loggly']['log_files'].empty? || !node['loggly']['log_dirs'].empty?,
+      :tags => node['loggly']['tags'].nil? || node['loggly']['tags'].empty? ? '' : "tag=\\\"#{node['loggly']['tags'].join("\\\" tag=\\\"")}\\\"",
+      :token => loggly_token
+    })
+    notifies :restart, "service[rsyslog]", :immediate
+  end
 end
